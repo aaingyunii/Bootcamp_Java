f9801e3169f32b29083e7248eed4ffd1b36511bb
aaingyunii <dlsrbs98@gmail.com>
2023-08-02 17:51:59 +0900

    java programming practice code

--- practiceExample/ForLab3.java
+++ practiceExample/ForLab3.java
@@ -6,6 +6,11 @@ public class ForLab3 {
 		// TODO Auto-generated method stub
 		int x = (int)(Math.random()*10)+1;
 		int y = (int)(Math.random()*11)+30;
+		/**
+		특정 범위의 난수 생성은 "범위의 최댓값" - max, "범위의 최솟값" - min
+		(Math.random()*(max-min)+1)+min => 이렇게 구한다.
+		**/
+		
 		int result=0;
 
 		
